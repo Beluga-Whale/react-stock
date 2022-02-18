baf1598158b5512adb5abd94cfe5904b3b209e46
Thanathat <74446225+Thanathat159@users.noreply.github.com>
2022-02-18 22:02:44 +0700

    final

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import { BrowserRouter } from 'react-router-dom'
+import GlobalStyle from './components/globalStyles'
 
 const App = () => {
   return (
     <>
-      <Navbar />
-      <Hero />
+      <BrowserRouter>
+        <GlobalStyle />
+        <Navbar />
+        <Hero />
+      </BrowserRouter>
+
     </>
   )
 }
